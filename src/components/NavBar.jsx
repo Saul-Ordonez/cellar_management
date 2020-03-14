1359f1e6da80390795428635a307448d72083d4c
Saul Ordonez <sauleordonez@gmail.com>
2020-03-14 12:02:40 -0700

    play with different color schemes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,9 +7,6 @@ function NavBar() {
   return (
     <div>
     <div class='navBarStyle'>
-    <div class='title'>
-    <h1>Cellar Mate</h1>
-    </div>
     <Link class='homeLink' to='/'>Home</Link>
     <Link class='listLink' to='/beerlist'>Cellar List</Link>
     <Link class='newBeerLink' to='/newbeer'>Add a Beer</Link>
