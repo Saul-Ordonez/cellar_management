5f77a3522368d8ff7a306359746f99b8d7acea1d
Saul Ordonez <sauleordonez@gmail.com>
2020-03-16 11:25:05 -0700

    continue editing card styling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,6 +4,11 @@ import './Landing.css';
 function Landing() {
   return(
     <div class='titleStyle'>
+    <h1>Welcome to Cellar Mate</h1>
+    <hr/>
+    <div class='landingContent'>
+    <h4>Keep track of your most precious craft beers! Here at Cellar Mate we understand that cellar management can be a bit daunting when it comes to knowing what you have to share or drink or continue to save. With Cellar Mate's online management system you can keep a live inventory of what you have in your cellar wherever you are so if you see that 2019 bottle of Founder's CBS at your local bottle shop and aren't quite sure if you picked one up yet, you can rest easy knowing what you have at all times.</h4>
+    </div>
     </div>
   );
 }
