44efd6ea3f9e4e9b3ce42b185a9c20f27355179c
Saul Ordonez <sauleordonez@gmail.com>
2020-03-16 10:23:57 -0700

    add styling to cards

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Beer from './Beer';
 import PropTypes from 'prop-types';
+import './Beer.css';
 
 function BeerList(props) {
 
   return (
-    <div>
+    <div class='card'>
     {props.beerList.map((beer, index) =>
       <Beer
       name={beer.name}
